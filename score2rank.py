72b96f6ffd25fa80be539a51caa3781cf42d8e32
Guilhem Piat <guilhempiat@yahoo.fr>
2017-12-04 16:22:21 +0100

    + added score2rank function
    args:
    m, a numpy matrix with 2 columns the 2nd of which should be numeric
    returns:
    m with the second column's numbers switched for ranks

---
+++ score2rank.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def score2rank(m):
+    """ args:
+            - m, a numpy matrix with 2 columns the 2nd of which should be numeric
+        returns:
+            - m with the second column's numbers switched for ranks
+        example:
+            m = [[3,5],    return = [[3,2],
+                 [4,1],              [4,5],
+                 [3,3],              [4,3],
+                 [8,7],              [4,1],
+                 [9,2]]              [4,4]]
+    """
+    m[np.argsort(m[:, 1], axis=0), 1] = [[i] for i in range(m.shape[0], 0, -1)]
+    return m
