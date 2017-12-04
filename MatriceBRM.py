61c683bf2528f804a3c16d70e05d15ed75dd678f
Aarboush <br.idriss@gmail.com>
2017-12-04 12:52:01 +0100

    création de la matrice BRM et listeTerme

--- MatriceBRM.py
+++ MatriceBRM.py
@@ -14,3 +14,13 @@ for i in range(3):
 		for element in range(1,542):
 			matriceBRM[colnum - 1+numeroColonne[i]][element-1] = sh.col(colnum)[element].value
 
+
+
+# Pour obtenir les indexs des termes de la matrice BRM
+listeTerme = []
+sh = classeur.sheet_by_name(nom_des_feuilles[0])
+for i in range(1,542):
+    listeTerme.append(sh.col(0)[i].value)
+
+#Exemple d'utilisation pour piocher la case entre ambulance et airplane de la matriceBRM
+print(matriceBRM[listeTerme.index('ambulance')][listeTerme.index('airplane')])
