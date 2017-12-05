811ae605e7f3aff230919807352222c0af46fd66
Guilhem Piat <guilhempiat@yahoo.fr>
2017-12-05 17:41:28 +0100

    clarified comments

--- score2rank.py
+++ score2rank.py
@@ -7,11 +7,11 @@ def score2rank(m):
         returns:
             - m with the second column's numbers switched for ranks
         example:
-            m = [[3,5],    return = [[3,2],
-                 [4,1],              [4,5],
-                 [3,3],              [3,3],
-                 [8,7],              [8,1],
-                 [9,2]]              [9,4]]
+            m = [[?,5],    return = [[?,2],
+                 [?,1],              [?,5],
+                 [?,3],              [?,3],
+                 [?,7],              [?,1],
+                 [?,2]]              [?,4]]
     """
     m[np.argsort(m[:, 1], axis=0), 1] = [[i] for i in range(m.shape[0], 0, -1)]
     return m
